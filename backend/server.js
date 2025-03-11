3ab17988509147f5acbef963897917103b5fdd1f
Rakesh Ranjan Kumar <107301763+RAKESH9798@users.noreply.github.com>
2025-03-11 15:43:06 +0530

    Update server.js
    locahost server updated with live server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://taskmanager-frontend-9n8s.onrender.com", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", protect, taskRoutes);
